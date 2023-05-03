b12e14a1d579b80c010c90a42db30eb3a2f43e85
Simon Reeves <simon@analogstudio.co.uk>
2023-05-03 12:01:26 +0100

    reads temps from dnmp module, made bat file to run via scheduled tasks

--- an_farmview/client/envmonitor.py
+++ an_farmview/client/envmonitor.py
@@ -1,12 +1,14 @@
 import requests
 import pprint
 
+import an_farmview.snmp
+    
 def post():
     url = 'http://127.0.0.1:5000/envmonitor'
 
     data = {
-        "temperature01": 99,
-        'temperature02': 99
+        "temperature01": an_farmview.snmp.get_temperature_rear(),
+        'temperature02': an_farmview.snmp.get_temperature_front()
         }
 
     r = requests.post(url, json=data)
